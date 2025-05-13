c2ace052d788ec8b2a5756f5faec52181d2be34c
xkyii@th <xkyii@foxmail.com>
2025-05-13 11:50:52 +0800

    Up: rapidee, pixpin

--- Script/install.py
+++ Script/install.py
@@ -6,17 +6,24 @@ PAF_DIR = 'D:\\Soft\\PortableApps\\'
 LAUNCHER_EXE = PAF_DIR + 'PortableApps.comLauncher\\PortableApps.comLauncherGenerator.exe'
 INSTALLER_EXE = PAF_DIR + 'PortableApps.comInstaller\\PortableApps.comInstaller.exe'
 
-def create_launcher_of_rapidee():
-    cmd = LAUNCHER_EXE + " " + APPS_DIR + "RapidEE"
+
+def create_launcher_of(app_name):
+    cmd = LAUNCHER_EXE + " " + APPS_DIR + app_name
     print(cmd)
     subprocess.run(cmd, shell=True)
 
-def create_installer_of_rapidee():
-    cmd = INSTALLER_EXE + " " + APPS_DIR + "RapidEE"
+def create_installer_of(app_name):
+    cmd = INSTALLER_EXE + " " + APPS_DIR + app_name
     print(cmd)
     subprocess.run(cmd, shell=True)
-
 
 if __name__ == '__main__':
-    create_launcher_of_rapidee()
-    create_installer_of_rapidee()
+    apps = [
+        # 'RapidEE',
+        # 'PixPin',
+        'TotalCommander',
+    ]
+
+    for app in apps:
+        create_launcher_of(app)
+        create_installer_of(app)
